0a4aedb7c0ddfaf3469932176805e419f6fe5527
Gu Yingbo <guyingbo@papayamobile.com>
2018-12-19 11:06:06 +0800

    utils

---
+++ tls/utils.py
@@ -0,0 +1,10 @@
+def pack_int(length: int, data: bytes) -> bytes:
+    return len(data).to_bytes(length, "big") + data
+
+
+def pack_list(length: int, iterable) -> bytes:
+    return pack_int(length, b"".join(data for data in iterable))
+
+
+def pack_all(length: int, iterable) -> bytes:
+    return pack_int(length, b"".join(obj.pack() for obj in iterable))
